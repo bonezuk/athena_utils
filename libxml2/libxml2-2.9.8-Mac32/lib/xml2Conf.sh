3dc9ed28814a39f518b247293247cc76dc998012
Stuart MacLean <stuart@blackomega.co.uk>
2018-05-27 12:35:56 +0100

    Add libupnp and libxml2 libraries, with MacOSX built.

---
+++ libxml2/libxml2-2.9.8-Mac32/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/bonez/Development/athena_utils/libxml2/libxml2-2.9.8-Mac32/lib"
+XML2_LIBS="-lxml2 -lz -L/usr/local/Cellar/xz/5.2.3/lib -llzma -lpthread  -liconv  -lm "
+XML2_INCLUDEDIR="-I/Users/bonez/Development/athena_utils/libxml2/libxml2-2.9.8-Mac32/include/libxml2"
+MODULE_VERSION="xml2-2.9.8"
+
