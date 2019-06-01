8502517b4bead6a60dcca37bc217f1f427f3389a
Stuart MacLean <stuart@blackomega.co.uk>
2019-06-01 20:07:48 +0100

    Recompile RPi2 libraries

--- libxml2/libxml2-2.9.8-RPi2/lib/xml2Conf.sh
+++ libxml2/libxml2-2.9.8-RPi2/lib/xml2Conf.sh
@@ -1,8 +1,8 @@
 #
 # Configuration file for using the XML library in GNOME applications
 #
-XML2_LIBDIR="-L/home/pi/Development/utils/athena_utils/libxml2/libxml2-2.9.8-RPi2/lib"
+XML2_LIBDIR="-L/home/pi/Development/athena_utils/libxml2/libxml2-2.9.8-RPi2/lib"
 XML2_LIBS="-lxml2 -lz -llzma    -lm "
-XML2_INCLUDEDIR="-I/home/pi/Development/utils/athena_utils/libxml2/libxml2-2.9.8-RPi2/include/libxml2"
+XML2_INCLUDEDIR="-I/home/pi/Development/athena_utils/libxml2/libxml2-2.9.8-RPi2/include/libxml2"
 MODULE_VERSION="xml2-2.9.8"
 
